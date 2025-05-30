51d7d10416721cd00353d4b6eae4f59bba9e6dda
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-05-30 06:39:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import Header from "./components/Header";
 function App() {
-  return <h1> </h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header id="header">
+      <img src="/src/assets/growing-money.svg" alt="Invest" />
+      <h1>Investment Calculator </h1>;
+    </header>
+  );
+}
+
+export default Header;
